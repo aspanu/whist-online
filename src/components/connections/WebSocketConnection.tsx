058fc8dd9cdbc1ae7a60b9f9496a7d83101cf6f9
Adrian Spanu <adri@nspanu.ca>
2021-05-19 22:34:57 -0700

    Adding the client side communication mechanism.

--- src/components/connections/WebSocketConnection.tsx
+++ src/components/connections/WebSocketConnection.tsx
@@ -1,4 +1,4 @@
-import { w3cwebsocket as W3CWebSocket } from "websocket";
+import { w3cwebsocket as WebSocket } from "websocket";
 import Card from "../../model/Card";
 
 /**
@@ -28,14 +28,14 @@ import Card from "../../model/Card";
  */
 
 interface Message { type: string; }
-interface StartMessage extends Message { start: string; }
+interface StartMessage extends Message { start: boolean; }
 interface BidMessage extends Message { bid: number; }
 interface CardMessage extends Message { card: Card; }
 
 class WebSocketClient {
     private static instance: WebSocketClient;
     
-    private client = new W3CWebSocket('ws://127.0.0.1:8080/game')
+    private client = new WebSocket('ws://127.0.0.1:8080/game')
 
     static get(): WebSocketClient {
         if (!this.instance) {
@@ -45,7 +45,64 @@ class WebSocketClient {
         return this.instance;
     }
 
-    //TODO: Add websocket message sending functions
+    sendStart() {
+        // Create the JSON version of the start message 
+        const message : StartMessage = { type : "start", start: true };
+        const jsonMessage : string = JSON.stringify(message);
+        
+        this.client.send(jsonMessage);
+    }
+
+    sendBid(bidAmount: number) {
+        const message : BidMessage = { type: "bid", bid: bidAmount };
+        const jsonMessage : string = JSON.stringify(message);
+
+        this.client.send(jsonMessage);
+    }
+
+    sendCard(cardToSend: Card) {
+        const message : CardMessage = { type: "card", card: cardToSend };
+        const jsonMessage : string = JSON.stringify(message);
+
+        this.client.send(jsonMessage);
+    }
+
+    getMessage(message: string) {
+        const messageObject : Message = JSON.parse(message);
+
+        switch (messageObject.type) {
+            case "start":
+                this.receiveStart();
+                break;
+
+            case "card":
+                const cardMessage : CardMessage = JSON.parse(message);
+                this.receiveCard(cardMessage.card);
+                break;
+
+            case "bid":
+                const bidMessage : BidMessage = JSON.parse(message);
+                this.receiveBid(bidMessage.bid);
+                break;
+
+            default:
+                throw TypeError(`Cannot parse websocket message: ${message}`)
+                break;
+        } 
+        
+    }
+
+    receiveStart() {
+        // TODO: add something here
+    }
+
+    receiveCard(card: Card) {
+        // TODO: add something here
+    }
+
+    receiveBid(bid: number) {
+        // TODO: add something here
+    }
 
     // componentDidMount() {
     //     client.onopen = () => {
